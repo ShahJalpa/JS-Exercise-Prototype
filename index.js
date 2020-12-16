792d240a89bbf82d7b1c0f0271089f61ed66c5c7
jalpa Shah <jalpa-shah@lambdastudents.com>
2020-12-16 17:08:38 -0500

    person and car tasks are done

--- index.js
+++ index.js
@@ -39,17 +39,25 @@ function Airplane(name) {
       - Give instances of Person a method `.toString()`:
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
-  
- function Person() {
+ function Person(name, age) {
+    this.name=name;
+    this.age=age;
+    this.stomach=[]; 
+  }
+  Person.prototype.eat = function(str){
+    if (this.stomach.length<=10){
+      this.stomach.push(str);
+    }
+  }
+  Person.prototype.poop = function(){
+    //this.stomach.splice(0, this.stomach.length);// this method works also. But this is too complicated. follow below method to reset or clear the array
+      this.stomach=[];
     
   }
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`;
+  }
  
- 
-
-  
-  
-  
-  
   /*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -64,10 +72,15 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
-  
+  Car.prototype.fill = function(gallons){
+    this.tank = this.tank + gallons;
+  }
   
   /*
     TASK 3
@@ -76,10 +89,13 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+    this.name = name;
+    this.age = age;
   }
- 
+ Baby.prototype.favoriteToy = function(){
+
+ }
   
   /* 
     TASK 4
